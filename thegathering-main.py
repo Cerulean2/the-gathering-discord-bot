fced447b5b880097d92f7d286a0d99ca39a58d98
Andrew :) <91349881+Cerulean2@users.noreply.github.com>
2023-04-07 23:00:51 -0400

    Update thegathering-main.py

--- thegathering-main.py
+++ thegathering-main.py
@@ -13,6 +13,7 @@ from dotenv import load_dotenv
 load_dotenv()
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='!', intents=intents)
+bot.remove_command('help')
 
 @bot.event
 async def on_ready():
